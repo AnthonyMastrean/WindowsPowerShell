661cf6074c25e1a0dd18b8374a7e211e368ea457
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-02-12 18:17:38 -0500

    stashing powershell user script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+Import-Module posh-git
+Import-Module PowerTab -ArgumentList "D:\Users\anthony\Documents\WindowsPowerShell\Modules\PowerTab\PowerTabConfig.xml"
+Import-Module Pscx
+
+$vcargs = ?: {$Pscx:Is64BitProcess} {'amd64'} {'x86'}
+$vcvars = "${env:VS100COMNTOOLS}..\..\VC\vcvarsall.bat"
+Invoke-BatchFile $vcvars $vcargs
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function prompt {
+    Write-Host($pwd) -nonewline
+    $Global:GitStatus = Get-GitStatus
+    Write-GitStatus $GitStatus
+    return "> "
+}
+
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+
+# Set up tab expansion and include git expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    switch -regex ($lastBlock) {
+        'git (.*)' { GitTabExpansion $lastBlock }
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+
+Enable-GitColors
+Clear-Host
