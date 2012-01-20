d5e8e18671ecffdcc09d1a7ccfc3497d70bfb737
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-01-20 10:56:15 -0500

    ah, better sections

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,19 +5,24 @@ Import-Module PowerTab -ArgumentList "$ENV:APPDATA\powertab\powertabconfig.xml"
 Import-Module Pscx
 Import-Module posh-git
 
+# ==================================================
 # Setup PSDrive for Scripts directory
-if(-not(Test-Path Scripts:))
-{
+# ==================================================
+if(-not(Test-Path Scripts:)) {
     $scripts = Join-Path (Split-Path $PROFILE) 'Scripts'
     New-PSDrive -name Scripts -PSProvider FileSystem -Root $scripts | Out-Null
 }
 
+# ==================================================
 # Include the Visual Studio tools
+# ==================================================
 $vcargs = ?: {$Pscx:Is64BitProcess} {'amd64'} {'x86'}
 $vcvars = "${ENV:VS100COMNTOOLS}..\..\VC\vcvarsall.bat"
 Invoke-BatchFile $vcvars $vcargs
 
+# ==================================================
 # Set up a simple prompt, adding the git prompt parts inside git repos
+# ==================================================
 function prompt 
 {
     $host.UI.RawUi.WindowTitle = ("{0}@{1}" -f $ENV:USERNAME, $ENV:COMPUTERNAME)
@@ -29,13 +34,17 @@ function prompt
     return "> "
 }
 
+# ==================================================
 # Setup PowerTab
+# ==================================================
 if(-not(Test-Path Function:\DefaultTabExpansion)) 
 {
     Rename-Item Function:\TabExpansion DefaultTabExpansion
 }
 
+# ==================================================
 # Setup tab expansion and include git expansion
+# ==================================================
 function TabExpansion($line, $lastWord) 
 {
     $lastBlock = [regex]::Split($line, '[|;]')[-1]
@@ -58,6 +67,5 @@ Register-EngineEvent -SourceIdentifier powershell.exiting -SupportEvent -Action 
 if(Test-Path $history) {
     Import-Csv $history | Add-History
 }
-# ==================================================
 
 Enable-GitColors
