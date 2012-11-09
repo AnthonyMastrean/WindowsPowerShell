c690047045b81482ed0f9759f0bd6f561076553a
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-11-09 16:16:39 -0500

    vs2012 is in

--- Functions/Invoke-VisualStudioTools.ps1
+++ Functions/Invoke-VisualStudioTools.ps1
@@ -3,11 +3,11 @@
 	Include the Visual Studio tools
 #>
 
-if(-not(Test-Path ENV:VS100COMNTOOLS)) {
+if(-not(Test-Path ENV:VS110COMNTOOLS)) {
 	Write-Error 'Cannot find the Visual Studio common tools'
 	return
 }
 
 $vcargs = ?: {$Pscx:Is64BitProcess} {'amd64'} {'x86'}
-$vcvars = "$ENV:VS100COMNTOOLS\..\..\VC\vcvarsall.bat"
+$vcvars = "$ENV:VS110COMNTOOLS\..\..\VC\vcvarsall.bat"
 Invoke-BatchFile $vcvars $vcargs
