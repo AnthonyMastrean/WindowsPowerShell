5186ee4dd81a773fd302c3fae040a2338bb9432f
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-12-11 19:38:11 -0500

    split the vs version

--- Functions/Invoke-VisualStudioTools.ps1
+++ Functions/Invoke-VisualStudioTools.ps1
@@ -1,13 +1,16 @@
 <# 
 	.SYNOPSIS
-	Include the Visual Studio tools
+	Include the Visual Studio tools using the Pscx
 #>
 
-if(-not(Test-Path ENV:VS100COMNTOOLS)) {
+$VERSION = 110
+$TOOLS = "ENV:VS{0}COMNTOOLS" -f $VERSION
+
+if(-not(Test-Path $TOOLS)) {
 	Write-Error 'Cannot find the Visual Studio common tools'
 	return
 }
 
 $vcargs = ?: {$Pscx:Is64BitProcess} {'amd64'} {'x86'}
-$vcvars = "$ENV:VS100COMNTOOLS\..\..\VC\vcvarsall.bat"
+$vcvars = "$TOOLS\..\..\VC\vcvarsall.bat"
 Invoke-BatchFile $vcvars $vcargs
