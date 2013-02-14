a7b206ebf6b7baafd8915c126d16e3c61582e6b5
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-13 21:34:33 -0500

    splitting some prompt sub-funcs

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,29 +7,6 @@ Get-Module -ListAvailable | ?{ $_.ModuleType -eq 'Script' } | Import-Module
 
 Get-ChildItem $here\Functions -include *.ps1 -recurse | %{ . $_.FullName }
 
-function Print-GitStatus {
-  $realLASTEXITCODE = $LASTEXITCODE
-  
-  $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-  Write-Host $pwd -NoNewLine
-  Write-VcsStatus
-  
-  $global:LASTEXITCODE = $realLASTEXITCODE
-  
-  '> '
-}
-
-function Set-SessionTitle {
-  $host.UI.RawUi.WindowTitle = ('{5}{0}@{1} [.NET {2}.{3}] ({4})' -f `
-      $ENV:USERNAME, `
-      $ENV:COMPUTERNAME, `
-      $PSVersionTable.CLRVersion.Major, `
-      $PSVersionTable.CLRVersion.Minor, `
-      $(if([IntPtr]::Size -eq 8){'amd64'} else{'x86'}),
-      $(if($host.UI.RawUI.WindowTitle.StartsWith('Administrator: ')){'Administrator: '})
-  )
-}
-
 function prompt {
   Set-VisitedDirectory
   Set-SessionTitle  
