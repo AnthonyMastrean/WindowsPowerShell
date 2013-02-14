2d6bbfcfa0219b55457eb68e5a9125bae43ffc01
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-13 21:32:43 -0500

    splitting some prompt sub-funcs

---
+++ Functions/Print-GitStatus.ps1
@@ -0,0 +1,11 @@
+function Print-GitStatus {
+  $realLASTEXITCODE = $LASTEXITCODE
+  
+  $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+  Write-Host $pwd -NoNewLine
+  Write-VcsStatus
+  
+  $global:LASTEXITCODE = $realLASTEXITCODE
+  
+  '> '
+}
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,28 +7,33 @@ Get-Module -ListAvailable | ?{ $_.ModuleType -eq 'Script' } | Import-Module
 
 Get-ChildItem $here\Functions -include *.ps1 -recurse | %{ . $_.FullName }
 
-function prompt {
-    Set-VisitedDirectory
+function Print-GitStatus {
+  $realLASTEXITCODE = $LASTEXITCODE
+  
+  $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+  Write-Host $pwd -NoNewLine
+  Write-VcsStatus
+  
+  $global:LASTEXITCODE = $realLASTEXITCODE
+  
+  '> '
+}
 
-    $host.UI.RawUi.WindowTitle = ('{5}{0}@{1} [.NET {2}.{3}] ({4})' -f `
-        $ENV:USERNAME, `
-        $ENV:COMPUTERNAME, `
-        $PSVersionTable.CLRVersion.Major, `
-        $PSVersionTable.CLRVersion.Minor, `
-        $(if([IntPtr]::Size -eq 8){'amd64'} else{'x86'}),
-        $(if($host.UI.RawUI.WindowTitle.StartsWith('Administrator: ')){'Administrator: '})
-    )
-    
-    $realLASTEXITCODE = $LASTEXITCODE
-    
-    # posh-git
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-    Write-Host $pwd -NoNewLine
-    Write-VcsStatus
-    
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    
-    return '> '
+function Set-SessionTitle {
+  $host.UI.RawUi.WindowTitle = ('{5}{0}@{1} [.NET {2}.{3}] ({4})' -f `
+      $ENV:USERNAME, `
+      $ENV:COMPUTERNAME, `
+      $PSVersionTable.CLRVersion.Major, `
+      $PSVersionTable.CLRVersion.Minor, `
+      $(if([IntPtr]::Size -eq 8){'amd64'} else{'x86'}),
+      $(if($host.UI.RawUI.WindowTitle.StartsWith('Administrator: ')){'Administrator: '})
+  )
+}
+
+function prompt {
+  Set-VisitedDirectory
+  Set-SessionTitle  
+  Print-GitStatus
 }
 
 Set-Alias new New-Object
