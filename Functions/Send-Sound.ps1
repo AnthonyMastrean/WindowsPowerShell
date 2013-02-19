73d5029f83b3636433f7792fc7907d7d370a2aa1
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-19 13:24:45 -0500

    helpers to send sounds, likely after a long command

---
+++ Functions/Send-Sound.ps1
@@ -0,0 +1,25 @@
+function Send-Sound { 
+
+  param(
+    [ValidateNotNullOrEmpty()]
+    [string] $name = 'Beep'
+  )
+
+  [System.Media.SystemSounds]::$name.Play()
+}
+
+function Send-Beep { 
+  Send-Sound -Name 'Beep'
+}
+
+function Send-Notify { 
+  Send-Sound -Name 'Exclamation'
+}
+
+function Send-Error { 
+  Send-Sound -Name 'Hand'
+}
+
+function Send-Warning { 
+  Send-Sound -Name 'Asterisk'
+}
