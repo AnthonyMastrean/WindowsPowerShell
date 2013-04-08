42ca63c1dbeae4caa343221cefb8ba522c497994
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-04-08 09:06:42 -0400

    love these source control extensions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,8 @@ $here = Split-Path $PROFILE
 Import-Module PowerTab -ArgumentList "$ENV:APPDATA\powertab\powertabconfig.xml"
 Import-Module Pscx
 Import-Module posh-git
+Import-Module posh-hg
+Import-Module posh-tf
 Import-Module Pester
 Import-Module trace-location
 Import-Module remember
