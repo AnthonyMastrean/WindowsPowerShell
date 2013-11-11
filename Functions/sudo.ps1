2218853473cc48546ae8f25148ad5661990c85e1
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-11-11 16:28:14 -0500

    using the sudo avail from lukesampson and scoop!

--- Functions/sudo.ps1
+++
@@ -1,8 +0,0 @@
-function sudo {
-  if ($args.Length -eq 1) {
-    Start-Process $args[0] -Verb "runAs"
-  }
-  if ($args.Length -gt 1) {
-    Start-Process $args[0] -ArgumentList $args[1..$args.Length] -Verb "runAs"
-  }
-}
