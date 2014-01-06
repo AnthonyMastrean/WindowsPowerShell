664dddfed483bba6833e544dd3a6529f44044787
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-06 13:48:33 -0500

    don't always load vsvars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,6 @@ $here = Split-Path $PROFILE
 Get-ChildItem $here\Modules -include *.psm1 -recurse | Import-Module
 Get-ChildItem $here\Functions -include *.ps1 -recurse | %{ . $_.FullName }
 
-Set-VsVars -version 11.0
-
 $global:GitPromptSettings.EnableWindowTitle = $false
 
 function prompt {
