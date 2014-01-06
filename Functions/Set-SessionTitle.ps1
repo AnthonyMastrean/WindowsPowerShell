83e991a66aca36bb80b732a2c91b3f8ea2d80d38
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-06 14:04:54 -0500

    dress up the window title

--- Functions/Set-SessionTitle.ps1
+++
@@ -1,8 +0,0 @@
-$host.UI.RawUi.WindowTitle = ('{5}{0}@{1} [.NET {2}.{3}] ({4})' -f `
-  $ENV:USERNAME, `
-  $ENV:COMPUTERNAME, `
-  $PSVersionTable.CLRVersion.Major, `
-  $PSVersionTable.CLRVersion.Minor, `
-  $(if([IntPtr]::Size -eq 8){'amd64'} else{'x86'}),
-  $(if($host.UI.RawUI.WindowTitle.StartsWith('Administrator: ')){'Administrator: '})
-)
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,17 @@ $here = Split-Path $PROFILE
 Get-ChildItem $here\Modules -include *.psm1 -recurse | Import-Module
 Get-ChildItem $here\Functions -include *.ps1 -recurse | %{ . $_.FullName }
 
+$host.UI.RawUi.WindowTitle = ('{5}{0}@{1}({4}) [PS {6}] [.NET {2}.{3}]' -f `
+  $ENV:USERNAME, `
+  $ENV:COMPUTERNAME, `
+  $PSVersionTable.CLRVersion.Major, `
+  $PSVersionTable.CLRVersion.Minor, `
+  $(if([IntPtr]::Size -eq 8){'x64'}else{'x86'}),
+  $(if($host.UI.RawUI.WindowTitle.StartsWith('Administrator: ')){'Administrator: '}),
+  $PSVersionTable.PSVersion
+)
+
+
 $global:GitPromptSettings.EnableWindowTitle = $false
 
 function prompt {
