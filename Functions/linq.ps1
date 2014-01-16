365ab813e5cf1b5d4bd5ccbe77279d8cd89645e6
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-16 14:54:12 -0500

    Provide a helpful Select-Top n records function

--- Functions/linq.ps1
+++ Functions/linq.ps1
@@ -6,5 +6,10 @@ function Select-Last {
   $input | Select-Object -Last 1
 }
 
+function Select-Top([int]$n = 10) {
+  $input | Select-Object -First $n
+}
+
 Set-Alias first Select-First
 Set-Alias last Select-Last
+Set-Alias top Select-Top
