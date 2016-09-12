7442c2d875d5c5df769379c328cfad3225f4575a
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-09-12 12:54:15 -0400

    [svn-clean] delete in sort order to avoid dir not found errors

--- Cmdlets/svn-clean.ps1
+++ Cmdlets/svn-clean.ps1
@@ -2,5 +2,6 @@ function svn-clean() {
     svn status `
         | ?{ $_ -match '(?<status>^\?)(?<whitespace>\s*)(?<filepath>.*)' } `
         | %{ $matches['filepath'] } `
+        | Sort-Object -Descending `
         | Remove-Item -Recurse -Force
 }
