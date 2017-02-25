bfd228323f103b07f13dbfefcba0c7a7658d9178
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-02-25 18:00:53 -0500

    [svn-clean] fix regex

--- Cmdlets/svn-clean.ps1
+++ Cmdlets/svn-clean.ps1
@@ -1,6 +1,6 @@
 function svn-clean() {
     svn status `
-        | ?{ $_ -match '(?<status>^\?)(?<whitespace>\s*)(?<filepath>.*)' } `
+        | ?{ $_ -match '(?<status>^\?)(?<whitespace>\s+)(?<filepath>.+$)' } `
         | %{ $matches['filepath'] } `
         | Sort-Object -Descending `
         | Remove-Item -Recurse -Force
