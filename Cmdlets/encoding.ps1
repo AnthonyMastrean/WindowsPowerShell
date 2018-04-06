1ab3d87d382809ab608b00c063b7bac22a743647
Anthony Mastrean <anthony.mastrean@vaeit.com>
2018-04-06 10:30:25 -0400

    add common encodings as ConvertTo/From cmdlets

---
+++ Cmdlets/encoding.ps1
@@ -0,0 +1,53 @@
+function ConvertTo-Base64 {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Text))
+}
+
+function ConvertFrom-Base64 {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($Text))
+}
+
+function ConvertTo-Html {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Net.WebUtility]::HtmlEncode($Text)
+}
+
+function ConvertFrom-Html {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Net.WebUtility]::HtmlDecode($Text)
+}
+
+function ConvertTo-Url {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Net.WebUtility]::UrlEncode($Text)
+}
+
+function ConvertFrom-Url {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Text
+    )
+
+    [System.Net.WebUtility]::UrlDecode($Text)
+}
