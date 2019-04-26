aadac186b60c1c25ff493634375bb10ec39d39f2
Anthony Mastrean <anthony.mastrean@gmail.com>
2019-04-26 12:20:17 -0400

    Remove poor quote matching

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,28 +19,6 @@ $GitPromptSettings.EnableWindowTitle = $true
 
 Set-PSReadlineOption -ExtraPromptLineCount 2
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
-Set-PSReadLineKeyHandler `
-    -Chord 'Oem7','Shift+Oem7' `
-    -BriefDescription SmartInsertQuote `
-    -LongDescription "Insert paired quotes if not already on a quote" `
-    -ScriptBlock {
-        param($key, $arg)
-
-        $line = $null
-        $cursor = $null
-        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
-        if ($line[$cursor] -eq $key.KeyChar) {
-            # Just move the cursor
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-        }
-        else {
-            # Insert matching quotes, move cursor to be in between the quotes
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
-            [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
-        }
-    }
 
 Set-Alias cal Write-Calendar
 Set-Alias fastdelete Remove-ItemNative
