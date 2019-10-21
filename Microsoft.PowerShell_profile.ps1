b4ad0f4975e9c464342fff6e16515c1bd35aca54
Anthony Mastrean <anthony.mastrean@gmail.com>
2019-10-21 11:50:14 -0400

    Update aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,30 +20,32 @@ $GitPromptSettings.EnableWindowTitle = $true
 
 # chocolatey
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path -Path $ChocolateyProfile) {
     Import-Module "$ChocolateyProfile"
 }
 
+# psreadline
 Set-PSReadlineOption -ExtraPromptLineCount 2
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
-Set-Alias cal Write-Calendar
-Set-Alias fastdelete Remove-ItemNative
-Set-Alias first Select-FirstObject
-Set-Alias httpcode Get-HttpStatusCode
-Set-Alias isup Test-IsWebsiteUp
-Set-Alias last Select-LastObject
-Set-Alias less more
-Set-Alias mklink New-SymbolicLink
-Set-Alias mnemonic Get-MnemonicName
-Set-Alias new New-Object
-Set-Alias play Send-Sound
-Set-Alias say Speak-Text
-Set-Alias size Write-ItemSize
-Set-Alias sunrise Get-Sunrise
-Set-Alias top Select-TopObject
-Set-Alias touch Set-FileTime
-Set-Alias unzip Expand-Archive
-Set-Alias uuidgen New-Guid
-Set-Alias watch Invoke-Watch
-Set-Alias which Get-Command
+# built-in aliases
+Set-Alias -Name less -Value more
+Set-Alias -Name mklink -Value New-Item
+Set-Alias -Name new -Value New-Object
+Set-Alias -Name unzip -Value Expand-Archive
+Set-Alias -Name uuidgen -Value New-Guid
+Set-Alias -Name which -Value Get-Command
+
+# custom aliases
+Set-Alias -Name cal -Value Write-Calendar
+Set-Alias -Name del -Value Remove-ItemNative -Option AllScope -Force
+Set-Alias -Name first -Value Select-FirstObject
+Set-Alias -Name http -Value Get-HttpStatusCodeInfo
+Set-Alias -Name last -Value Select-LastObject
+Set-Alias -Name mnemonic -Value Get-MnemonicName
+Set-Alias -Name play -Value Send-Sound
+Set-Alias -Name say -Value Speak-Text
+Set-Alias -Name size -Value Write-ItemSize
+Set-Alias -Name top -Value Select-TopObject
+Set-Alias -Name touch -Value Set-FileTime
+Set-Alias -Name watch -Value Invoke-Watch
