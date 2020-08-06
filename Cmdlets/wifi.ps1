30299ddf5fa85cfafd74dfaea0e6bc69cb531107
Anthony Mastrean <anthony.mastrean@gmail.com>
2020-08-06 12:01:43 -0400

    Update wifi.ps1

--- Cmdlets/wifi.ps1
+++ Cmdlets/wifi.ps1
@@ -10,10 +10,8 @@ function Show-WifiStatus {
 
     $wifiSymbol = [char]::ConvertFromUtf32(0x26A1)
 
-    $interfaces = Get-NetAdapter -Physical | ?{ $_.PhysicalMediaType -eq 'Native 802.11' } | ?{ $_.Status -eq 'Up' }
+    $interfaces = Get-NetAdapter -Physical | ? { $_.PhysicalMediaType -eq 'Native 802.11' } | ? { $_.Status -eq 'Up' }
     $chunk = [System.Math]::Ceiling($percent / 25) # 0..4
 
-    $display += "{0} {1,3}% [{2}{3}]" -f $wifiSymbol, $percent, (')' * $chunk), (' ' * (4 - $chunk))
-
-    $display
+    "{0} {1,3}% [{2}{3}]" -f $wifiSymbol, $percent, (')' * $chunk), (' ' * (4 - $chunk))
 }
